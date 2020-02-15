e2ecaf84613c7f33503b54daf7ea525532d109d9
s0553405@htw-berlin.de <s0553405@htw-berlin.de>
2020-02-15 15:40:43 +0100

    some issues fixed

--- movieService/src/main/java/com/movieService/movieService/MovieServiceApplication.java
+++ movieService/src/main/java/com/movieService/movieService/MovieServiceApplication.java
@@ -1,10 +1,7 @@
 package com.movieService.movieService;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @EnableCircuitBreaker
