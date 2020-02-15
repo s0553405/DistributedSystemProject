3cb2b4051ff97c7b5ffc6f78734b5e640a8c4f53
s0553405@htw-berlin.de <s0553405@htw-berlin.de>
2020-02-15 16:25:50 +0100

    Anpassungen

--- movieService/src/main/java/com/movieService/movieService/MovieServiceApplication.java
+++ movieService/src/main/java/com/movieService/movieService/MovieServiceApplication.java
@@ -3,8 +3,10 @@ package com.movieService.movieService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableCircuitBreaker
+@EnableEurekaClient
 @SpringBootApplication
 public class MovieServiceApplication {
 
